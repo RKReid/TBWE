fc7ede82d3b5fb83427322a0ebe01e1ae84ca16c
RKReid <57508113+RKReid@users.noreply.github.com>
2020-03-13 16:13:19 -0400

    Add files via upload

---
+++ downloads/virus/The Coronavirus Virus.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+      System.out.println("YOUR COMPUTER HAS A VIRUS!!!" );
+      System.out.println("YOUR COMPUTER HAS BEEN HACKED!!!" );
+      System.out.println("Windows ERROR:6583   SHUTTING DOWN!" );
+     System.out.println("" );
+     System.out.println("ERROR" );  
+        System.out.println("CANNOT CLOSE" );
+        System.out.println("" );
+        System.out.println("ERROR" );
+        System.out.println("" );
+        System.out.println("" );
+        System.out.println("The Coronavirus has taken over your computer." );
+Scanner scanner = new Scanner(System.in); 
+scanner.nextLine();
+
+    }
+}
