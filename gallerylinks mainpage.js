7cf8c971e14c531c1e89083fbc3167ff704de94e
RKReid <57508113+RKReid@users.noreply.github.com>
2021-01-11 08:40:00 -0500

    Create gallerylinks mainpage.js

---
+++ gallerylinks mainpage.js
@@ -0,0 +1,4 @@
+var page1="<a class='a1' style='cursor:progress' href= 'gallery.html'>Page 1</a>";
+var page2="<a class='a1' style='cursor:progress' href= 'gallery2.html'>Page 2</a>";
+document.write(page1);
+document.write(page2);
