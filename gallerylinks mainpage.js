de5ffa7fec5e54ad4cd2a56203ab86d88d308705
RKReid <robkreid05@gmail.com>
2021-02-18 10:14:43 -0500

    ￣\_(”ノ)_/￣

--- gallerylinks mainpage.js
+++ gallerylinks mainpage.js
@@ -1,6 +1,8 @@
 var page1="<a class='a1' style='cursor:progress' href= 'gallery.html'>Page 1</a>";
 var page2="<a class='a1' style='cursor:progress' href= 'gallery2.html'>Page 2</a>";
 var page3="<a class='a1' style='cursor:progress' href= 'gallery3.html'>Page 3</a>";
+var page4="<a class='a1' style='cursor:progress' href= 'gallery4.html'>Page 4</a>";
 document.write(page1);
 document.write(page2);
 document.write(page3);
+document.write(page4);
